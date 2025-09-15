d87029da5964b173b0b3d6111c4cb3e644353a9d
joshieman <71846165+joshieman06@users.noreply.github.com>
2025-09-15 11:03:05 -0700

    Remove logic and add datapack and mod files

--- common/src/main/java/com/finndog/mmv/events/lifecycle/TagsUpdatedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.finndog.mmv.events.lifecycle;
-
-import com.finndog.mmv.events.base.EventHandler;
-import net.minecraft.core.RegistryAccess;
-
-public record TagsUpdatedEvent(RegistryAccess registryAccess, boolean fromPacket) {
-
-    public static final EventHandler<TagsUpdatedEvent> EVENT = new EventHandler<>();
-}
